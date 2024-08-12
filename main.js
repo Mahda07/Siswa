2a478e39880bf32da47c27a6c709e7af720ae976
Mahda07 <mahdafatihah@gmail.com>
2024-08-12 10:54:38 +0700

    mengimpor library firestore

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js'
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js'
+import {
+  getFirestore,
+  collection,
+  doc,
+  getDocs,
+  getDoc,
+  addDoc,
+  deleteDoc,
+  updateDoc,
+  query,
+  orderBy
+} from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js'
 
 
 const firebaseConfig = {
